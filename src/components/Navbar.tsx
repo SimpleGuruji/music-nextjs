f988682380a8f7d7f06a0ab5c72cda344eacdf7c
Simple Guruji <dashingak957014@gmail.com>
2024-03-01 14:37:45 +0530

    Hero section part completed.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,7 @@ function Navbar({ className }: { className?: string }) {
             <HoveredLink href="/courses">Music Production</HoveredLink>
           </div>
         </MenuItem>
-        <Link href={"/"}>
+        <Link href={"/contact"}>
           <MenuItem
             setActive={setActive}
             active={active}
